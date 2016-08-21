df8f03c405d036a9373aa9f3e5cc7aa73c5b4d6b
Jeroen Arnoldus <jeroen@repleo.nl>
2016-08-21 13:29:43 +0200

    Fixed serialization bug

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ else:
 
 setup(
     name='django-roa',
-    version='2.0.1',
+    version='2.0.2',
     url='https://github.com/bjarnoldus/django-roa',
     download_url='https://github.com/bjarnoldus/django-roa/archive/master.zip',
     license='BSD',
